b1f928de6706dcb130b17c50bc186903ca79b80c
neominal.solutions <neominal.solutions@gmail.com>
2024-04-30 15:27:24 +0300

    Sorgu

--- MongoDbDriverSampleApp/Controllers/StudentsController.cs
+++ MongoDbDriverSampleApp/Controllers/StudentsController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.Core.WireProtocol.Messages;
 using MongoDbDriverSampleApp.Documents;
+using MongoDbDriverSampleApp.Dtos;
 using MongoDbDriverSampleApp.Settiings;
 
 namespace MongoDbDriverSampleApp.Controllers
@@ -73,7 +75,60 @@ namespace MongoDbDriverSampleApp.Controllers
     [HttpGet]
     public async Task<IActionResult> Get()
     {
-      return Ok();
+
+      // many to many relations select sorgusu
+      // $TeacherIds tabloadaki FK alanına bağlandığımız ad
+      // teacherIds uygulama içerisinde kullandığımız içerisinde FK gelen değerleri In sorgıusuna soktuğumuz değişken.
+      var pipeline = new BsonDocument[]
+      {
+        new BsonDocument("$lookup", new BsonDocument()
+        {
+          { "from","teachers" },
+          { "let", new BsonDocument("teacherIds","$TeacherIds") },
+          { "pipeline", new BsonArray
+          {
+            new BsonDocument("$match", new BsonDocument()
+            {
+              {"$expr", new BsonDocument("$in", new BsonArray
+                {
+                 "$_id","$$teacherIds"
+                })
+              }
+            })
+          } 
+          },
+          {"as","teachers" } // document içerisine teachers olarak getir.
+        }),
+             // $map ile dizi içerisinde dönüp tek tek itemları güncelleyebiliriz. 
+                  // $addFields ile yeni bir alan açtık 
+        new BsonDocument("$addFields", new BsonDocument
+        {
+          { "Teachers", new BsonDocument("$map", new BsonDocument
+          {
+            { "input","$teachers" }, // dizinin mapten önceki hali
+            {"as", "teacher" }, // dizideki her bir item
+            {"in", new BsonDocument // dizinin içine girip tek tek değerleri transform edebiliriz
+            {
+              {"_id", new BsonDocument("$toString","$$teacher._id") },
+              {"Name", "$$teacher.Name" }
+            }}
+          })
+          }
+        }),
+        new BsonDocument("$project", new BsonDocument
+        {
+          {"_id", new BsonDocument("$toString","$_id") },
+          {"Name",1 },
+          {"ClassName",1 },
+          {"Teachers", "$Teachers"}
+         
+        })
+      };
+
+
+      var doc = await studentCollection.Aggregate<StudentWithTeacherDto>(pipeline).ToListAsync();
+
+      return Ok(doc);
     }
   }
 }
---
+++ MongoDbDriverSampleApp/Dtos/StudentWithTeacherDto.cs
@@ -0,0 +1,19 @@
+﻿namespace MongoDbDriverSampleApp.Dtos
+{
+  public class TeacherDto
+  {
+    public string Id { get; set; }
+    public string Name { get; set; }
+
+  }
+  public class StudentWithTeacherDto
+  {
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string ClassName { get; set; }
+
+    public List<TeacherDto> Teachers { get; set; } = new List<TeacherDto>();
+
+
+  }
+}
